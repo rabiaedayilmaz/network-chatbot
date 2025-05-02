927c6661f25e8fa9f428b4c44ccbd98fee298911
rabiaedayilmaz <rabiaedayilmaz@gmail.com>
2025-05-02 09:00:25 +0300

    hotfix(agent): hypernet

--- llm/utils/tools/hypernet_funcs.py
+++ llm/utils/tools/hypernet_funcs.py
@@ -2,7 +2,7 @@ import speedtest
 from utils.log import logger
 
 
-async def run_speed_test() -> str | None:    
+async def run_speed_test(query: str) -> str | None:    
     try:
         st = speedtest.Speedtest()
         st.get_best_server()
@@ -10,7 +10,8 @@ async def run_speed_test() -> str | None:
         upload_speed = st.upload() / 1_000_000      # Mbps
         ping = st.results.ping
 
-        speedtest_results = f"""Internet Speed Test Results:
+        speedtest_results = f"""User: {query}
+        Internet Speed Test Results:
         - Download Speed: {download_speed:.2f} Mbps
         - Upload Speed: {upload_speed:.2f} Mbps
         - Ping: {ping:.2f} ms
